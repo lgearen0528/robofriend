c11129c8e5d131c967694dc8d867afedb48a9085
Gearen Liem J <GearenLiemJ@JohnDeere.com>
2020-01-24 14:44:21 -0600

    added font and started components and containers

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="tc">
+        <h1 className="f1">Robofriends</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
